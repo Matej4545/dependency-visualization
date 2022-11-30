e51ab0d7e8c71f85c9570db49c212f613d2f4645
Matěj Groman <matej.groman98@gmail.com>
2022-11-30 17:57:18 +0100

    WIP: Better graph UI

--- src/depvis-next/components/Graph/NoSSRGraph.tsx
+++ src/depvis-next/components/Graph/NoSSRGraph.tsx
@@ -1,26 +1,54 @@
 import dynamic from 'next/dynamic';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import ReactForceGraph2d, { ForceGraphMethods } from 'react-force-graph-2d';
+import { forceCollide, forceLink, forceManyBody, forceX, forceY } from 'd3-force';
+import { SonatypeOSSVulnFetcher } from '../../vulnerability-mgmt/SonatypeOSSVulnFetcher';
+
+const my_map = (value, x1, y1, x2, y2) => ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
 
 export default function NoSSRGraph(props) {
+  const maxDepsCount = 262;
   const graphRef = useRef<ForceGraphMethods>();
+  const f = new SonatypeOSSVulnFetcher();
 
+  useEffect(() => {
+    const r = graphRef.current;
+    r.zoomToFit();
+    r.d3Force('collide', forceCollide(4));
+    r.d3Force('x', forceX());
+    r.d3Force('y', forceY());
+    r.d3Force('link')
+      .distance((link) => (1 * (link.source.deps_count + link.target.deps_count * 2)) / maxDepsCount)
+      // .distance((link) => 1000)
+      .strength(1);
+    r.d3Force('charge', forceManyBody().strength(-70));
+  }, []);
   const handleClick = useCallback(
     (node) => {
       // Aim at node from outside it
-      console.log(node);
       graphRef.current.centerAt(node.x, node.y, 500);
       let zoom_level = 3 - node.deps_count / 100;
       if (zoom_level < 0.5) zoom_level = 0.5;
       graphRef.current.zoom(zoom_level, 500);
-      console.log(zoom_level);
+      console.log(node.neighbors);
+      // f.fetchInfo(node.id);
     },
     [graphRef]
   );
   return (
     <ReactForceGraph2d
       ref={graphRef}
+      nodeCanvasObjectMode={() => 'after'}
+      nodeCanvasObject={(node, ctx, globalScale) => {
+        const label = `${node.name} | ${node.deps_count}`;
+        const fontSize = my_map(node.deps_count, 0, maxDepsCount, 5, 22);
+        ctx.font = `${fontSize}px Sans-Serif`;
+        ctx.fillStyle = '#121212';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(label, node.x, node.y);
+      }}
       {...props}
       linkDirectionalArrowLength={5}
       linkDirectionalArrowRelPos={3}
@@ -28,7 +56,9 @@ export default function NoSSRGraph(props) {
       nodeVal={'deps_count'}
       // backgroundColor={'#f0f0f0'}
       linkColor={'#ff0000'}
-      onNodeClick={handleClick}
+      onNodeClick={(n) => {
+        handleClick(n);
+      }}
     />
   );
 }
--- src/depvis-next/pages/toolbox.tsx
+++ src/depvis-next/pages/toolbox.tsx
@@ -1,4 +1,33 @@
+import { useState } from 'react';
+import { Container, Table } from 'react-bootstrap';
+
 const Toolbox = () => {
-  return <p>Nothing to see here</p>;
+  return <DetailsList></DetailsList>;
 };
 export default Toolbox;
+
+const mockupData = [
+  { Key: 'a', value: '1' },
+  { Key: 'b', value: '1' },
+  { Key: 'c', value: '1' },
+];
+
+const DetailsList = () => {
+  const [data, setData] = useState(mockupData);
+
+  return (
+    <Container>
+      Details:
+      <Table striped hover>
+        {data.map((d) => {
+          return (
+            <tr>
+              <td>{d.Key}</td>
+              <td>{d.value}</td>
+            </tr>
+          );
+        })}
+      </Table>
+    </Container>
+  );
+};
